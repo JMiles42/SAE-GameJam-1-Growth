3f4e52fc2976b33d488ea3a600a09a20eda09c76
Unknown <J@jmiles42.com>
2018-08-30 17:49:17 +1000

    v0.1

--- Assets/Scripts/Player/PlayerScaleManager.cs
+++ Assets/Scripts/Player/PlayerScaleManager.cs
@@ -34,17 +34,17 @@ public class PlayerScaleManager: FoCsBehaviour
 
 	private void ValueChange()
 	{
-		if(ScaleRef.Value < 20)
+		//if(ScaleRef.Value < 20)
+		//{
+		//	tween?.Complete();
+		//	tween            = transform.DOPunchScale(Vector3.one * ScaleRef.Value.Clamp(0.5f, 20), ScaleSpeed, 7);
+		//	tween.onComplete -= OnComplete;
+		//	tween.onComplete += OnComplete;
+		//}
+		//else
 		{
 			tween?.Complete();
-			tween            = transform.DOPunchScale(Vector3.one * ScaleRef.Value.Clamp(0.5f, 20), ScaleSpeed, 7);
-			tween.onComplete -= OnComplete;
-			tween.onComplete += OnComplete;
-		}
-		else
-		{
-			tween?.Complete();
-			transform.DOScale(Vector3.one * ScaleRef.Value.Clamp(0.5f, 20), 1);
+			tween = transform.DOScale(Vector3.one * ScaleRef.Value.Clamp(0.5f, 20), 1);
 		}
 
 		WorldSettings.GameRadius = Lerps.Lerp(35f, 60f, ScaleRef.Value.Clamp(0, 2));
--- Assets/Scripts/WorldObject/WorldObject.cs
+++ Assets/Scripts/WorldObject/WorldObject.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using ForestOfChaosLib;
 using ForestOfChaosLib.AdvVar;
 using UnityEngine;
 
 public class WorldObject: FoCsRigidbodyBehaviour
 {
-	public event Action<Motor> OnPlayerInteraction;
-	public bool                RotateOnSpawn;
-	public FloatVariable       ScaleIncreaseAmount = 0.1f;
-	public BoolVariable        DealsDamage         = false;
-	public IntVariable         ScoreValue          = 10;
+	public static List<WorldObject> WorldObjects = new List<WorldObject>();
+	public event Action<Motor>      OnPlayerInteraction;
+	public bool                     RotateOnSpawn;
+	public FloatVariable            ScaleIncreaseAmount = 0.1f;
+	public BoolVariable             DealsDamage         = false;
+	public IntVariable              ScoreValue          = 10;
+
+	protected virtual void OnEnable()
+	{
+		WorldObjects.Add(this);
+	}
+
+	protected virtual  void OnDisable()
+	{
+		WorldObjects.Remove(this);
+	}
 
 	private void OnCollisionEnter(Collision collision)
 	{
